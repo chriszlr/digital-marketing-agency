b089fc0f0a0f027decd47a5f87a97d8eb6f2bdd9
Christopher Zöllner <chriszoelln@gmail.com>
2023-05-18 12:19:59 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,123 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="max-w-[1040px] mx-auto grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-8 md:p-6 p-4 pb-6">
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-xl my-3">Lorem, ipsum.</h3>
+
+        <ul className="flex justify-center items-center flex-col gap-1">
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-xl my-3">Lorem, ipsum.</h3>
+
+        <ul className="flex justify-center items-center flex-col gap-1">
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-xl my-3">Lorem, ipsum.</h3>
+
+        <ul className="flex justify-center items-center flex-col gap-1">
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-xl my-3">Lorem, ipsum.</h3>
+
+        <ul className="flex justify-center items-center flex-col gap-1">
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-xl my-3">Lorem, ipsum.</h3>
+
+        <ul className="flex justify-center items-center flex-col gap-1">
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-xl my-3">Lorem, ipsum.</h3>
+
+        <ul className="flex justify-center items-center flex-col gap-1">
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+          <li className="text-gray-200 hover:text-white cursor-pointer duration-300 text-lg">
+            Lorem, Ipsum.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
