fe15760422c73a68434a4d913260cfd7beb4485f
Christopher Zöllner <chriszoelln@gmail.com>
2023-05-18 12:54:47 +0200

    improvements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,16 +6,16 @@ const Hero = () => {
     <div className="max-w-[1200px] mx-auto p-4 flex flex-col md:flex-row justify-between items-center overflow-hidden">
       <div className="">
         <div className="flex flex-col justify-between">
-          <h1 className="font-light text-3xl md:text-3xl lg:text-4xl flex flex-col my-1">
+          <h1 className="font-light text-3xl md:text-3xl lg:text-6xl flex flex-col my-1">
             Sales Optimization <span>& Data Analysis</span>
           </h1>
 
-          <p className="font-light text-xs lg:text-md xl:text-lg mb-2 mt-1">
+          <p className="font-light text-xs lg:text-md xl:text-xl mb-2 mt-1">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
             distinctio inventore magnam nihil officiis!
           </p>
         </div>
-        <button className="bg-orange-400 rounded-full py-1 px-2 text-xs font-medium hover:bg-orange-500 duration-300 my-2">
+        <button className="bg-orange-400 rounded-full py-1 px-2 md:text-lg font-medium hover:bg-orange-500 duration-300 my-2">
           View Work -{">"}
         </button>
       </div>
